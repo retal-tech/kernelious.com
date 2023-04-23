d49a6185a096fa096c8bf4bb2154758c6721ac77
Arian Ebrahimi <ae@retal.tech>
2023-04-23 04:19:06 +0330

    Update

--- backend/charlotte/settings.py
+++ backend/charlotte/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites.models.Site',
     'rest_framework',
     'corsheaders',
     'blog',
